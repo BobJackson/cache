174e3c7f596dea3da17dfa43a059999fa9de70dd
Bob Jackson <wangyousong@9191offer.com>
2023-06-23 20:22:54 +0800

    Update cache config

--- src/main/java/com/wangyousong/component/cache/config/CacheConfig.java
+++ src/main/java/com/wangyousong/component/cache/config/CacheConfig.java
@@ -1,13 +1,22 @@
 package com.wangyousong.component.cache.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.DefaultBaseTypeLimitingValidator;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
 
 import static org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 
 @Configuration
@@ -18,8 +27,20 @@ public class CacheConfig {
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(connectionFactory)
-                .cacheDefaults(defaultCacheConfig())
+                .cacheDefaults(customCacheConfiguration())
                 .build();
     }
 
+    private RedisCacheConfiguration customCacheConfiguration() {
+        return defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(serializer()));
+    }
+
+    private static GenericJackson2JsonRedisSerializer serializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
 }
